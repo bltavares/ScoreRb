babb59d54a5cf36b36a8aecc47090ca82f268cb0
Bruno Tavares <bruno.exz@gmail.com>
2011-03-16 23:19:46 -0300

    Added some tests and implemented

--- spec/scorerb_spec.rb
+++ spec/scorerb_spec.rb
@@ -2,26 +2,34 @@ require 'spec_helper'
 
 describe Scorerb do
 	it "should add score method to strings" do
-		String.should respond_to :score
+		"".should respond_to :score
 	end
 	
 	it "should return 1 for exact match" do
-		"Hello".score("Hello").should be 1.0	
+		"Hello".score("Hello").should be == 1.0	
 	end
 	
 	it "should return 0 for no matches" do
-		"Hello".score("abc").should be 0
-	end
-
-	it "should return 0.5 for half match" do
-		"Hi there".score("Hi t").should be 0.5
+		"Hello".score("abc").should be == 0
 	end
 
 	it "should'n return 0 on semi-correct match with fuzzines on" do
 		"Hi there".score("Hi thi", 0.7).should be > 0
 	end
 
-	it "should drop score with higher fuzzines" do
-		"Hi there".score("Hi thi", 0.1).should be > "Hi there".score("Hi thi", 0.8)
+	it "should score more points starting the string" do
+		"Hi there".score("Hi t").should be > "Hi there".score("here")
+	end
+
+	it "should score more points if the star letter is at same case" do
+		"Hi there".score("Hi there").should be > "Hi there".score("hi there")
+	end
+	
+	specify "acronyms should have better scores" do
+		"Hillsdale Michigan".score("HiMi").should be > "Hillsdale Michigan".score("Hills")
+	end
+
+	it "should drop score with lower fuzzines" do
+		"Hi there".score("Hi thi", 0.1).should be < "Hi there".score("Hi thi", 0.8)
 	end
 end
